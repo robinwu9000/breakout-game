7e95d90f56766e089f1d637935f86e0e3d9e5ce9
Robin Wu <robwu@ucdavis.edu>
2015-08-25 17:40:45 -0700

    Paddle and input added

--- game.js
+++ game.js
@@ -8,17 +8,33 @@ var dy = -2;
 
 var ballRadius = 10;
 
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth)/2;
+
+var rightPressed = false;
+var leftPressed = false;
+
 function draw() {
   ctx.clearRect(0,0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
   x += dx;
   y += dy;
   requestAnimationFrame(draw);
+
   if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
+
   if(y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
     dy = -dy;
+  }
+
+  if(rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7;
+  } else if(leftPressed && paddleX > 0) {
+    paddleX -= 7;
   }
 }
 
@@ -28,6 +44,33 @@ function drawBall() {
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
+}
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#259a3b";
+  ctx.fill();
+  ctx.closePath();
+}
+
+$(document).keydown(keyDownHandler);
+$(document).keyup(keyUpHandler);
+
+function keyDownHandler(event) {
+  if(event.keyCode == 39) {
+    rightPressed = true;
+  } else if(event.keyCode == 37) {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(event) {
+  if(event.keyCode == 39) {
+    rightPressed = false;
+  } else if(event.keyCode == 37) {
+    leftPressed = false;
+  }
 }
 
 requestAnimationFrame(draw);
